7f808d9eda993713091d35b881f46d1ec1f9384c
Aris Ripandi <ar.is@outlook.com>
2020-12-15 17:27:45 +0700

    Add headlessui and heroicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { ChevronRightSolid } from '@graywolfai/react-heroicons'
 import Head from 'next/head'
 import SiteLayout from '../components/site-layout'
 
@@ -24,19 +25,8 @@ export default function Home() {
               href="https://github.com/riipandi/next-tailwind-starter"
               className="inline-flex items-center px-8 py-3 font-sans text-base font-medium text-white transition duration-500 ease-in-out transform border border-transparent rounded-md shadow-sm hover:scale-110 bg-gradient-to-r from-pink-500 to-orange-500 hover:from-orange-500 hover:to-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:from-orange-500 focus:to-pink-500"
             >
-              <svg
-                className="w-5 h-5 mr-2 -ml-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span>back to project page</span>
+              <span>Back to project page</span>
+              <ChevronRightSolid className="w-6 h-6 ml-1 -mr-1" />
             </a>
           </div>
         </div>
