f321ad3896db6e92b75037316ef549455b58c168
Aris Ripandi <aris@stmikpasim.ac.id>
2021-01-17 17:37:01 +0700

    Update site-layout.js

--- src/components/site-layout.js
+++ src/components/site-layout.js
@@ -4,7 +4,11 @@ import Head from 'next/head'
 const SiteLayout = ({ children }) => (
   <div className="overflow-x-hidden bg-gradient-to-r from-primary-500 to-secondary-500">
     <Head>
-      <link rel="icon" href="/favicon.ico" />
+      {/* Favicon */}
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" type="image/x-icon" sizes="16x16 32x32" href="/favicon.ico" />
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
     {children}
