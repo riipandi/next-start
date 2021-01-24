f8a70cc1a0f07031ed47db318fc5b110002f36c0
Aris Ripandi <aris@stmikpasim.ac.id>
2021-01-24 21:59:01 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Barlow Semi Condensed', 'Inter', ...defaultTheme.fontFamily.sans]
+        sans: ['Barlow Semi Condensed', ...defaultTheme.fontFamily.sans]
       },
       colors: {
         gray: defaultColor.coolGray,
