40d9125d7fdc3f00f3d8709d75146d617d1dbb6e
Aris Ripandi <ar.is@outlook.com>
2021-02-13 05:57:12 +0700

    Add framer motion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { ChevronRightSolid } from '@graywolfai/react-heroicons'
+import { motion } from 'framer-motion'
 import Head from 'next/head'
 import SiteLayout from '../components/site-layout'
 
@@ -10,9 +11,20 @@ export default function Home() {
       </Head>
       <div className="flex items-center min-h-screen text-white">
         <div className="container flex flex-wrap items-center p-4 mx-auto">
-          <div className="w-full p-4 text-center md:w-5/12">
+          <motion.div
+            className="w-full p-4 text-center md:w-5/12"
+            initial={{ scale: 0 }}
+            animate={{ rotate: -10, scale: 1 }}
+            whileHover={{ scale: 1.2, rotate: 2 }}
+            whileTap={{ scale: 1, rotate: 0, borderRadius: '100%' }}
+            transition={{
+              type: 'spring',
+              stiffness: 260,
+              damping: 20
+            }}
+          >
             <img src="/dev-animate.svg" alt="Welcome" />
-          </div>
+          </motion.div>
           <div className="w-full p-8 font-sans text-center md:w-7/12 md:text-left md:p-12">
             <div className="text-6xl font-bold tracking-wide">Hello</div>
             <div className="my-4 text-xl font-semibold md:text-3xl">
