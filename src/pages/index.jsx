51fee2acf504c58d5ac74e7b9b0b71164dc939d8
Aris Ripandi <ar.is@outlook.com>
2021-08-16 09:52:04 +0700

    Add eslint and use Next Image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import { ChevronRightSolid } from '@graywolfai/react-heroicons'
 import { motion } from 'framer-motion'
 import Head from 'next/head'
 import SiteLayout from '../components/site-layout'
+import Image from 'next/image'
+import devAnimate from '../assets/images/dev-animate.svg'
 
 export default function Home() {
   return (
@@ -21,22 +23,16 @@ export default function Home() {
               type: 'spring',
               stiffness: 260,
               damping: 20
-            }}
-          >
-            <img src="/dev-animate.svg" alt="Welcome" />
+            }}>
+            <Image src={devAnimate} alt="Welcome" />
           </motion.div>
           <div className="w-full p-8 font-sans text-center md:w-7/12 md:text-left md:p-12">
             <div className="text-6xl font-bold tracking-wide">Hello</div>
-            <div className="my-4 text-xl font-semibold md:text-3xl">
-              We are still build something amazing for you.
-            </div>
-            <div className="mb-8 text-lg md:text-2xl">
-              Nothing to see here yet! Please come back soon.
-            </div>
+            <div className="my-4 text-xl font-semibold md:text-3xl">We are still build something amazing for you.</div>
+            <div className="mb-8 text-lg md:text-2xl">Nothing to see here yet! Please come back soon.</div>
             <a
               href="https://github.com/riipandi/next-tailwind-starter"
-              className="inline-flex items-center px-8 py-3 font-sans text-base font-medium text-white transition duration-500 ease-in-out transform rounded-md shadow-sm hover:scale-110 bg-gradient-to-r from-primary-500 to-secondary-500 hover:from-secondary-500 hover:to-primary-500 focus:outline-none"
-            >
+              className="inline-flex items-center px-8 py-3 font-sans text-base font-medium text-white transition duration-500 ease-in-out transform rounded-md shadow-sm hover:scale-110 bg-gradient-to-r from-primary-500 to-secondary-500 hover:from-secondary-500 hover:to-primary-500 focus:outline-none">
               <span>Back to project page</span>
               <ChevronRightSolid className="w-6 h-6 ml-1 -mr-1" />
             </a>
