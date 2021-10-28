e04a52cd39c425b6b144bc3b5e3efe0e61ecc40b
Aris Ripandi <ar.is@outlook.com>
2021-10-28 12:37:34 +0700

    Remove Google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', 'Clarity City', ...defaultTheme.fontFamily.sans],
         slab: ['Sriracha', ...defaultTheme.fontFamily.sans]
       },
       screens: {
