010c2c6e21b8e4d9b6534b0eb4f4bfe9b35ca058
Aris Ripandi <ar.is@outlook.com>
2021-10-28 12:06:56 +0700

    Remove SplitBee

--- next.config.js
+++ next.config.js
@@ -14,18 +14,6 @@ const nextConfig = {
   },
   eslint: {
     dirs: ['src']
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/bee.js',
-        destination: 'https://cdn.splitbee.io/sb.js'
-      },
-      {
-        source: '/_hive/:slug',
-        destination: 'https://hive.splitbee.io/:slug'
-      }
-    ]
   }
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app'
-import Script from 'next/script'
 import { ThemeProvider } from 'next-themes'
 
 import '@/assets/fonts/_font_sans.css'
@@ -10,8 +9,6 @@ import '@/assets/styles/global.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
-      {/* Example SplitBee analytics integration using Next Script */}
-      <Script src='/bee.js' strategy='lazyOnload' />
       <Component {...pageProps} />
     </ThemeProvider>
   )
