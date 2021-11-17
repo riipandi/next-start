35cbe9b8726c10ba3f790acd4542dc34a05e5b8f
Aris Ripandi <ar.is@outlook.com>
2021-11-17 22:39:02 +0700

    Fix font CDN cloudflare

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
           <meta name='msapplication-TileImage' content='/favicon/ms-icon-144x144.png' />
           <meta name='theme-color' content='#ffffff' />
           {/* Load fonts from CDN */}
-          {/* <link rel='stylesheet' href='https://gfont.e90.workers.dev/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' /> */}
+          {/* <link rel='stylesheet' href='https://gfont.arsitec.workers.dev/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' /> */}
           <link rel='stylesheet' href='https://fonts.cdnfonts.com/css/inter' />
           <link rel='stylesheet' href='https://fonts.cdnfonts.com/css/sriracha' />
         </Head>
