63131abb68f7525afc7b60950d047bbf845020d6
Aris Ripandi <ar.is@outlook.com>
2022-02-11 21:52:13 +0700

    remove sample dashboard page

--- components/app-layout/index.ts
+++
@@ -1,7 +0,0 @@
-import Breadcrumbs from './Breadcrumbs'
-import PageLayout from './PageLayout'
-import SearchBox from './SearchBox'
-import Sidebar from './Sidebar'
-import Topbar from './Topbar'
-
-export { Breadcrumbs, PageLayout, SearchBox, Sidebar, Topbar }
