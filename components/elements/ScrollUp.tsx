7eb395e35b0903cbad6fa9a466dcfc5702093b23
Aris Ripandi <ar.is@outlook.com>
2022-03-29 15:15:15 +0700

    fix ScrollUp component position

--- components/elements/ScrollUp.tsx
+++ components/elements/ScrollUp.tsx
@@ -7,11 +7,11 @@ const ScrollUp = () => {
   const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
 
   return (
-    <div className='fixed bg-black opacity-75 cursor-pointer'>
+    <div className='fixed z-30 bg-black opacity-75 cursor-pointer'>
       {isScrolled && (
         <div
           onClick={scrollToTop}
-          className='fixed inline-flex items-center px-3 py-2 text-xs text-white transition duration-500 ease-in-out transform bg-black rounded cursor-pointer lg:bottom-5 lg:right-6 hover:bg-opacity-90 hover:-translate-y-1 hover:scale-110'
+          className='fixed inline-flex items-center px-3 py-2 text-xs text-white transition duration-500 ease-in-out transform bg-black rounded cursor-pointer bottom-5 right-6 hover:bg-opacity-90 hover:-translate-y-1 hover:scale-110'
         >
           Back to top
           <ArrowUp strokeWidth={2} className='w-4 h-4 ml-1 -mr-1' />
